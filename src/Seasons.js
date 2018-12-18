5088c47d3589817639ab490708f3359255fcb807
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-17 19:22:20 -0800

    added in ternearys for seasons

--- src/Seasons.js
+++ src/Seasons.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 
 class Seasons extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            lat: null,
-        }
+    state = { lat: null, errorMessage: ''}
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                console.log(position)
-            this.setState({ lat: position.coords.latitude})
-        }, (err) => console.log(err))
+            this.setState({ lat: position.coords.latitude})},
+            (err) => this.setState({errorMessage: err.message})
+       )
     }
+
+    getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+    }
+
+    
     render() {
+        const season = this.getSeason(this.state.lat, new Date().getMonth());
+        const icon = season === 'winter' ? 'snowflake' : 'sun';
         if(this.state.errorMessage && !this.state.lat) {
             return <div style={{background: 'grey', height: '100vh'}}>Error: {this.state.errorMessage}</div>
         }
@@ -22,6 +33,9 @@ class Seasons extends Component {
                 <div style={{background: 'grey', height: '100vh'}}>
                     <h1 style={{textAlign: 'center'}}>Seasons</h1>
                 <h3 style={{textAlign: 'center'}}>lattitude: {this.state.lat}</h3>
+                <i className={`${icon} icon`}/>
+                <h3>{season === 'winter' ? "Burrr it's Chilly!" : 'Lets hit the beach'}</h3>
+                <i className={`${icon} icon`}/>
                 </div>
             ) 
         }
