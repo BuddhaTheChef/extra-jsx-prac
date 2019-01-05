cfd7f1345d1dc955d7d26fc23510c814a60c8557
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-01-05 14:28:58 -0800

    refactored seasons app to use hooks

--- src/Seasons.js
+++ src/Seasons.js
@@ -1,70 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Sesons.css';
 // import Loader from './Loader';
+import SeasonsDisplay from './SeasonsDisplay';
+import UseLocation from './UseLocation';
 
-class Seasons extends Component {
+const Seasons = () => {
+    const [lat, errorMessage] = UseLocation();
 
-    state = { 
-        lat: null, 
-        lon: null,
-        errorMessage: ''
-    }
+    let content;
+    if(errorMessage) {
+        content = <div>Error: {errorMessage}</div>
+    } else if(lat) {
+        content = <SeasonsDisplay lat={lat} />
+    } 
 
-    seasonConfig = {
-        summer: {
-            text: 'Let\'s hit the beach',
-            iconName: 'sun'
-        },
-        winter: {
-            text: 'Burrrr it\'s cold',
-            iconName: 'snowflake'
-        }
-    }
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-            this.setState({ lat: position.coords.latitude, lon: position.coords.longitude})},
-            (err) => this.setState({errorMessage: err.message})
-       )
-    }
-
-    getSeason = (lat, month) => {
-    if(month > 2 && month < 9) {
-        return lat > 0 ? 'summer' : 'winter';
-    }
-    else {
-        return lat > 0 ? 'winter' : 'summer';
-    }
-    }
-
-
-    
-    render() {
-          const season = this.getSeason(this.state.lat, new Date().getMonth());
-        const { text, iconName } = this.seasonConfig[season];
-        if(this.state.errorMessage && !this.state.lat) {
-            return <div style={{background: 'grey', height: '100vh'}}>Error: {this.state.errorMessage}</div>
-        }
-        if(!this.state.errorMessage && this.state.lat) {
-            return(
-                <div className={`season-display ${season}`}>
-                    <h1 style={{textAlign: 'center'}}>Seasons</h1>
-                <h3 style={{textAlign: 'center'}}>lattitude: {this.state.lat}</h3>
-                <h3 style={{textAlign: 'center'}}>longitude: {this.state.lon}</h3>
-                <i className={`icon-left massive ${iconName} icon`}/>
-                <h1>{text}</h1>
-                <i className={`icon-right massive ${iconName} icon`}/>
-                </div>
-            ) 
-        }
-       else { return (
-        <div>
-            {/* <Loader message="Please wait for location request"/> */}
-        </div>
-        )
-       }
-    }
+    return <div className="border green">{content}</div>
 }
 
 export default Seasons;
